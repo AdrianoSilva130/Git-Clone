c00076aa15c3434f03bf27709cbdd815c325da57
Adriano Silva <adrianosilva130@gmail.com>
2024-11-22 01:18:26 -0300

    Exercício Git-Clone

--- cypress/e2e/Loja-teste/Login.cy.js
+++ cypress/e2e/Loja-teste/Login.cy.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
 
-describe('Funcionalidade: Login', () => {
+describe('Funcionalidade: login', () => {
 
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    });
+    })
 
     it('Deve fazer login com sucesso', () => {
+
         cy.get('#username').type('adriano.teste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, adriano.teste (não é adriano.teste? Sair)')
     });
     
-});
-
+    });
