18540e5044193300ebe711caea818df8852fb0b1
Adriano Silva <adrianosilva130@gmail.com>
2024-11-22 00:34:55 -0300

    Exercício Git-Clone

---
+++ cypress/e2e/Loja-teste/Produtos.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+
+        it('Deve selecionar um produto da lista', () => {
+            
+        });
+    
+    
+
+
+
+        
+
+
+        
+})
+});
