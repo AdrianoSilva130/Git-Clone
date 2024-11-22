23a30702023e2e06f2751cc91bc23f98b177c2ba
Adriano Silva <adrianosilva130@gmail.com>
2024-11-21 23:29:49 -0300

    Automação do site Americanas

---
+++ cypress/1-Loja-Americanas/Login.cy.js
@@ -0,0 +1,13 @@
+/// <references types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cypress.visit('https://cliente.americanas.com.br/minha-conta/entrar?next=https://www.americanas.com.br/')
+
+
+        
+    });
+    
+});
+
